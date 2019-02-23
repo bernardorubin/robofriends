94cf4ab9d123542d2f0dfb4d3e8b751d903d63f3
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-02-22 20:26:22 -0600

    Changed Component into a function

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Hello.css'
 
-export default class Hello extends Component {
-  render() {
-    const { greeting } = this.props
-    return (
-      <div className="f1 tc">
-        <h1>Hellow</h1>
-        <p>World</p>
-        <p>{greeting}</p>
-      </div>
-    )
-  }
+const Hello = ({ greeting }) => {
+  return (
+    <div className="f1 tc">
+      <h1>Hellow</h1>
+      <p>World</p>
+      <p>{greeting}</p>
+    </div>
+  )
 }
+
+export default Hello
