6be2318ba86bcb32ae2be29d4410853018ab3da1
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-02-22 20:23:44 -0600

    Receiving props

--- src/Hello.js
+++ src/Hello.js
@@ -3,10 +3,12 @@ import './Hello.css'
 
 export default class Hello extends Component {
   render() {
+    const { greeting } = this.props
     return (
       <div className="f1 tc">
         <h1>Hellow</h1>
         <p>World</p>
+        <p>{greeting}</p>
       </div>
     )
   }
