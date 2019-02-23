5577330014b0ca4c16d2f4e56d116800c52b429b
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-02-22 20:15:19 -0600

    Changed defult component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Hello.css'
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hellow</h1>
+        <p>World</p>
+      </div>
+    )
+  }
+}
