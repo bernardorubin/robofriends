0d88955f64b5e2baa4d1db9040b04ae52000cfa4
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-02-23 20:44:42 -0600

    Added Styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots'
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -16,14 +17,15 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const { robots } = this.state
+    const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase())
     })
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
